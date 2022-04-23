a6dca83b89d70bf459972406afe0e56cecd72737
gsashish <gsashishemail@gmail.com>
2022-04-23 12:03:24 +0530

    Initialize project using Create React App

---
+++ data.js
@@ -0,0 +1,15 @@
+const project = {
+  javascript: [
+    {
+      name: "meme generator",
+      link: "https://github.com/Briyani-monster/meme-generator",
+    },
+    {
+      name: "Todo List",
+      description:
+        "a To-do list App that have create,delete,edit and update. Application have three state for task which are not sttarted pending completed states and you can move you task by dragging into sections",
+      link: "https://briyani-monster.github.io/TO-DO_LIST/",
+      languages: ["Javascript", " HTML Drag and Drop API"],
+    },
+  ],
+};
